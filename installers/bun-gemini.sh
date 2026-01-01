853ffcd50220e700167dd5284bd3f94ade9b33a9
artyom <error1001es@gmail.com>
2026-01-01 23:05:26 +0300

    updated

--- installers/bun-gemini.sh
+++ installers/bun-gemini.sh
@@ -1,7 +1,7 @@
 curl -fsSL https://bun.sh/install | bash
 source $HOME/.bashrc
 bun install -g @google/gemini-cli@latest
-export PATH="$HOME/.bun/bin:$PATH"
 source $HOME/.bashrc
 sudo ln -s /usr/bin/bun /usr/bin/node
 source $HOME/.bashrc
+sudo ln -s /usr/bin/gemini "$HOME/.bun/bin/gemini"
