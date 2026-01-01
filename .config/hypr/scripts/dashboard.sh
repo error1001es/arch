1f729ddeaf1c4448b74fe096d29567f23dd29b2b
artyom <error1001es@gmail.com>
2026-01-01 21:47:58 +0300

    updated

--- .config/hypr/scripts/dashboard.sh
+++ .config/hypr/scripts/dashboard.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Запускаем Kitty с уникальным классом, прозрачностью и удерживаем окно открытым
 kitty --class="dashboard" --title="Dashboard" --hold sh -c "fastfetch"
-kitty --hold sh -c "v2rayN"
