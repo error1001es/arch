6ada4d666aec083132f07849484b98a670ede34b
artyom <error1001es@gmail.com>
2026-01-03 01:07:56 +0300

    updated

--- installers/cursor.sh
+++ installers/cursor.sh
@@ -2,3 +2,6 @@ cd /tmp && wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7
 tar -xvf Bibata-Modern-Ice.tar.xz
 mkdir -p ~/.local/share/icons
 mv Bibata-Modern-Ice ~/.local/share/icons/
+hyprpm update
+hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
+hyprpm enable dynamic-cursors
