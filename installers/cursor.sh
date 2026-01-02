06e77b679565796a2a63393415c5c09d18c8212c
artyom <error1001es@gmail.com>
2026-01-03 00:48:16 +0300

    updated

--- installers/cursor.sh
+++ installers/cursor.sh
@@ -1,4 +1,4 @@
-cd /tmp && wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice-Right.tar.xz
-tar -xvf Bibata-Modern-Ice-Right.tar.xz
+cd /tmp && wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
+tar -xvf Bibata-Modern-Ice.tar.xz
 mkdir -p ~/.local/share/icons
-mv Bibata-Modern-Ice-Right ~/.local/share/icons/
+mv Bibata-Modern-Ice ~/.local/share/icons/
